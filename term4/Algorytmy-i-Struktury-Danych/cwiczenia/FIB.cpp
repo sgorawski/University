a9a9f8cdb3af1a1c3d0350a845ea1ea26a0313ec
Sławomir Górawski <sgorawski@gmail.com>
2018-06-24 18:21:45 +0200

    And this one

--- term4/Algorytmy-i-Struktury-Danych/cwiczenia/FIB.cpp
+++ term4/Algorytmy-i-Struktury-Danych/cwiczenia/FIB.cpp
@@ -3,7 +3,8 @@
 
 using Matrix = std::array<std::array<long long, 2>, 2>;
 
-Matrix mtxMultMod(Matrix a, Matrix b, int m) {
+Matrix mtxMultMod(Matrix a, Matrix b, int m)
+{
 	Matrix res;
 	res[0][0] = (a[0][0]*b[0][0] + a[0][1]*b[1][0]) % m;
 	res[0][1] = (a[0][0]*b[0][1] + a[0][1]*b[1][1]) % m;
@@ -12,7 +13,8 @@ Matrix mtxMultMod(Matrix a, Matrix b, int m) {
 	return res;
 }
 
-Matrix fastMtxPowMod(Matrix a, int exp, int m) {
+Matrix fastMtxPowMod(Matrix a, int exp, int m)
+{
 	if (exp == 0)
 		return {{{1, 0}, {0, 1}}};
 	Matrix x = fastMtxPowMod(a, exp/2, m);
@@ -20,14 +22,15 @@ Matrix fastMtxPowMod(Matrix a, int exp, int m) {
 	return (exp % 2 == 0 ? x : mtxMultMod(a, x, m));
 }
 
-int fibMod(int n, int m) {
+int fibMod(int n, int m)
+{
 	Matrix fib_mtx = {{{1, 1}, {1, 0}}};
 	fib_mtx = fastMtxPowMod(fib_mtx, n - 1, m);
 	return fib_mtx[0][0];
 }
 
-
-int main() {
+int main()
+{
 	int t;
 	scanf("%d", &t);
 
